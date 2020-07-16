92aa5c921d458868373c54f4888b2862da435c8d
Frison Alexander <frialex@gmail.com>
2020-07-16 15:09:10 +0000

    lab spring JDBC template

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
     @Bean
-    public TimeEntryRepository getTimeEntryRepo(){
-        return new InMemoryTimeEntryRepository();
+    public TimeEntryRepository getTimeEntryRepo(DataSource ds){
+        return new JdbcTimeEntryRepository(ds);
     }
 
     public static void main(String[] args){
