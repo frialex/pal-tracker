b9212d2d88e89c5bc15f94170fa34ae2c3896ff3
Frison Alexander <frialex@gmail.com>
2020-07-16 19:05:52 +0000

    lab - health check - add component tag

--- src/main/java/io/pivotal/pal/tracker/TimeEntryHealthIndicator.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryHealthIndicator.java
@@ -5,6 +5,7 @@ import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
+@Component
 public class TimeEntryHealthIndicator implements HealthIndicator {
     private static final int MAX_TIME_ENTRIES = 5;
     private final TimeEntryRepository timeEntryRepo;
